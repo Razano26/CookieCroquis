284d1cb518075de74a94c0f059b2db3e6e086f41
Louis Labeyrie <labeyrielouis@gmail.com>
2025-06-29 10:34:57 +0200

    feat: Add Docker support with Dockerfile and .dockerignore
    - Introduced a Dockerfile for building and running the application in a containerized environment.
    - Added a .dockerignore file to exclude unnecessary files from the Docker context.
    - Configured Next.js for standalone output in next.config.js.
    
    Signed-off-by: Louis Labeyrie <labeyrielouis@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Page() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold underline">Cookie Croquis</h1>
+    </div>
+  );
 }
